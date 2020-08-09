1655c97620879e12aa22b398a183e0882871c07c
Daniel Lwetabe <acehanks@gmail.com>
2020-08-09 19:27:35 +0300

    fix failing test for CalcClient class

--- src/test/java/com/example/calc/CalcClientTest.java
+++ src/test/java/com/example/calc/CalcClientTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 class CalcClientTest {
 
@@ -12,8 +14,12 @@ class CalcClientTest {
 
     @Test
     public void getAddResponse() {
-        AddResponse response;
-        response = client.getAddResponse();
+        AddResponse response = new AddResponse();
+        response.setAddResult(3);
+
+        CalcClient clientMock = mock(CalcClient.class);
+        when(clientMock.getAddResponse()).thenReturn(response);
+
         assertEquals(3, response.getAddResult());
     }
 }
